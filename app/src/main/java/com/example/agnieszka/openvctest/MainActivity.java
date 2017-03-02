9d965a1c69ebe0486779a2db4ca2ce87b8d3c8a6
Unknown <jobagnieszka@gmail.com>
2017-03-03 00:06:12 +0100

    Add background removal

--- app/src/main/java/com/example/agnieszka/openvctest/MainActivity.java
+++ app/src/main/java/com/example/agnieszka/openvctest/MainActivity.java
@@ -23,8 +23,15 @@ import org.opencv.android.OpenCVLoader;
 import org.opencv.core.Core;
 import org.opencv.core.CvType;
 import org.opencv.core.Mat;
+import org.opencv.core.MatOfFloat;
+import org.opencv.core.MatOfInt;
+import org.opencv.core.Point;
+import org.opencv.core.Scalar;
 import org.opencv.core.Size;
 import org.opencv.imgproc.Imgproc;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity implements CvCameraViewListener2 {
 
@@ -40,10 +47,13 @@ public class MainActivity extends AppCompatActivity implements CvCameraViewListe
 
     // These variables are used (at the moment) to fix camera orientation from 270degree to 0degree
     Mat mRgba;
-    Mat mRgbaF;
-    Mat mRgbaT;
+    Mat mHSV;
+    Mat mHist;
     Mat mGray;
     Mat mCanny;
+    Mat mTthres;
+
+
     static{
         System.loadLibrary("MyOpencvLibs");
     }
@@ -117,10 +127,11 @@ public class MainActivity extends AppCompatActivity implements CvCameraViewListe
     public void onCameraViewStarted(int width, int height) {
 
         mRgba = new Mat(height, width, CvType.CV_8UC4);
-        mRgbaF = new Mat(height, width, CvType.CV_8UC4);
-        mRgbaT = new Mat(width, width, CvType.CV_8UC4);
+        mHSV = new Mat(height, width, CvType.CV_8UC3);
+        mHist = new Mat(width, width, CvType.CV_8UC1);
         mGray = new Mat(height,width, CvType.CV_8UC1);
         mCanny = new Mat(height,width, CvType.CV_8UC1);
+        mTthres = new Mat(height,width, CvType.CV_8UC1);
     }
 
     //destroy image data when you stop camera preview
@@ -132,16 +143,48 @@ public class MainActivity extends AppCompatActivity implements CvCameraViewListe
 
         // TODO Auto-generated method stub
         mRgba = inputFrame.rgba();
+        //Canny detect
         Imgproc.cvtColor(mRgba, mGray,Imgproc.COLOR_RGB2GRAY);
         Imgproc.blur(mGray, mGray, new Size(3,3));
         OpencvNativeClass.cannyDetect(mGray.getNativeObjAddr(), mCanny.getNativeObjAddr());
-        //Imgproc.Canny(mGray, mGray, 30, 90);
+        //Background removal
+        Imgproc.cvtColor(mRgba, mHSV,Imgproc.COLOR_BGR2HSV);
+        List<Mat> hsvMat = new ArrayList<>();
+        Core.split(mHSV, hsvMat);
+        MatOfInt histRange = new MatOfInt(180);
+        //hsvMat.get(0); //hue mat
+        Imgproc.calcHist(hsvMat, new MatOfInt(0), new Mat(), mHist, histRange, new MatOfFloat(0, 179));
+/*
+public static void calcHist(java.util.List<Mat> images,
+                            MatOfInt channels,
+                            Mat mask,
+                            Mat hist,
+                            MatOfInt histSize,
+                            MatOfFloat ranges)
+
+ */
+        double average = 0.0;
+        for (int h = 0; h < 180; h++)
+        {
+            // for each bin, get its value and multiply it for the corresponding
+            // hue
+            average += (mHist.get(h, 0)[0] * h);
+        }
+        average = average / mHSV.size().height / mHSV.size().width;
+        Imgproc.threshold(hsvMat.get(0), mTthres,average, 179.0, Imgproc.THRESH_BINARY );
+        Imgproc.blur(mTthres, mTthres, new Size(5, 5));
+        Imgproc.dilate(mTthres, mTthres, new Mat(), new Point(-1, -1), 1);
+        Imgproc.erode(mTthres, mTthres, new Mat(), new Point(-1, -1), 3);
+        Imgproc.threshold(mTthres, mTthres, average, 179.0, Imgproc.THRESH_BINARY);
+        Mat foreground = new Mat(mRgba.size(), CvType.CV_8UC3, new Scalar(255, 255, 255));
+        mRgba.copyTo(foreground, mTthres);
+
         // Rotate mRgba 90 degrees
        // Core.transpose(mRgba, mRgbaT);
        // Imgproc.resize(mRgbaT, mRgbaF, mRgbaF.size(), 0,0, 0);
         //Core.flip(mRgbaF, mRgba, 1 );
 
-        return mCanny; // This function must return
+        return foreground; // This function must return
     }
 
 }
