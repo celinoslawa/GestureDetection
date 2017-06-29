dd850322220a0ea7954856f76ef6b5a60d26968a
celinoslawa <jobagnieszka@gmail.com>
2017-06-29 20:18:24 +0200

    Revert "refactor of code"
    This reverts commit 17f762c3964327c41817ecaa2f4d84a8510a71c9.

--- openCVLibrary320/build/generated/source/aidl/debug/org/opencv/engine/OpenCVEngineInterface.java
+++ openCVLibrary320/build/generated/source/aidl/debug/org/opencv/engine/OpenCVEngineInterface.java
@@ -1,6 +1,6 @@
 /*
  * This file is auto-generated.  DO NOT MODIFY.
- * Original file: C:\\Users\\eagnjob\\Magisterka\\GestureDetection\\openCVLibrary320\\src\\main\\aidl\\org\\opencv\\engine\\OpenCVEngineInterface.aidl
+ * Original file: /home/agnieszka/AndroidStudioProjects/Openvctest/openCVLibrary320/src/main/aidl/org/opencv/engine/OpenCVEngineInterface.aidl
  */
 package org.opencv.engine;
 /**
